72502346ca2ccf1a820c400eba7a687891f94fee
selva1234567854 <142561278+selva1234567854@users.noreply.github.com>
2023-08-18 10:42:02 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//menu
+var tombolMenu = $(".tombol-menu")
+var menu = $("nav .menu ul")
+
+function klikMenu(){
+    tombolMenu.click (function(){
+        menu.toggle();
+    });
+    menu.click(function(){
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if(width < 990) {
+        klikMenu();
+    }
+   
+})
